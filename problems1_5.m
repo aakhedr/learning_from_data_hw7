695407d5a324b7c02e1a6a6c4e075765966d1530
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2014-11-15 14:14:11 +0200

    Done with problem 2

--- problems1_5.m
+++ problems1_5.m
@@ -1,13 +1,19 @@
-%% problem 1
+%% problems 1, 2
 data = load('train.txt');
 % training
 X = data(1:25, 1:2);            y = data(1:25, 3);
 % validation
 Xval = data(26:end, 1:2);       yVal = data(26:end, 3);
+% test set
+test = load('test.txt');
+Xtest = test(:, 1:2);           yTest = test(:, 3);
 % linear transform
 X = transform(X);               Xval = transform(Xval);
-fprintf('\tk\t\tEin\t\tEval\n');
+Xtest = transform(Xtest);
+
+fprintf('\tk\t\tEin\t\tEval\t\tEout\n');
 for i = 3:7
+    % train each model
     Xtrain = X(:, 1:i);         m = size(Xtrain, 1);
     Xtrain = [ones(m, 1) Xtrain];
     w = pinv(Xtrain' * Xtrain) * Xtrain' * y;
@@ -15,13 +21,21 @@ for i = 3:7
     
     Ein = length(y_train_est(y_train_est~=y))/ length(y);
     
+    % validate each model
     Xvalidate = Xval(:, 1:i);   n = size(Xvalidate, 1);
     Xvalidate = [ones(n, 1) Xvalidate];
-    
     y_val_est = sign(Xvalidate * w);
+    
     Eval = length(y_val_est(y_val_est~=yVal))/ length(yVal);
     
-    fprintf('\t%f\t%f\t%f\n', i, Ein, Eval);
+    % check each model with against test set (out of sample)
+    XtestSet = Xtest(:, 1:i);      l = size(XtestSet, 1);
+    XtestSet = [ones(l, 1) XtestSet];
+    y_test_est = sign(XtestSet * w);
+    
+    Eout = length(y_test_est(y_test_est~=yTest))/ length(yTest);
+    
+    fprintf('\t%f\t%f\t%f\t%f\n', i, Ein, Eval, Eout);
 end
     
     
